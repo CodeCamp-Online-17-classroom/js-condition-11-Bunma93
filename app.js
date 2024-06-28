f183fcfc95990b0f4580d49f309b15ef9bf1d11d
Bunma93 <bunma93@gmail.com>
2024-06-28 10:49:53 +0700

    Js-condition11

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // write code here
+let user = prompt("ชื่อผู้ใช้");
+if (!user) {
+    user = "guest"
+} else if (user == "codecamp") {
+   passWord = prompt("กรอกพาสเวิร์ด")
+   if (passWord == "123456") {
+    user = "codecamp"
+   } else {
+    alert("Wrong password")
+    user = "guest"
+   }
+} else {
+    user = "guest"
+};
+alert(`Welcome ${user}`);
